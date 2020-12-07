2ea80e0a6ee76a55b95d408b04fda60d51a53d4c
Lina Sachuk <sachuklina@gmail.com>
2020-12-07 15:05:28 -0700

    add persistStore

--- react-ecosystems/src/index.js
+++ react-ecosystems/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
 import App from './App.js';
 
+const store = configureStore();
+
+
+
 ReactDOM.render(
-    <Provider store={configureStore()}>
+    <Provider store={store}>
         <App />
     </Provider>,
     document.getElementById('root'),
